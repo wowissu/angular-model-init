4c9448a9a4a5f8ae2a7b782d56466f3d1e7e2eaa
wowissu <bbgbbg0123@gmail.com>
2016-03-08 23:56:52 +0800

    push readme

--- README.md
+++ README.md
@@ -6,6 +6,21 @@ let ng-model can init
 
 ## Example
 
+```js
+app.directive('ngModelInit', ['$parse', function ($parse) {
+    return {
+        require: 'ngModel',
+        restrict: 'A',
+        link: function ($scope, $element, $attrs, ngModel) {
+            ngModel.$setViewValue($parse($attrs.ngModelInit)($scope));
+            ngModel.$render();
+            $scope.$destroy();
+        }
+    };
+}]);
+```
+
+
 ```html
 <input ng-model="myModel" ng-model-init="'123456'" />
 <input ng-model="myModel" ng-model-init="[1, 2, 3]" />
--- angular-model-init.js
+++ angular-model-init.js
@@ -1,17 +1,17 @@
 (function (angular) {
-  
-  var app = angular.module('ngModelInit');
 
-  app.directive('ngModelInit', ['$parse', function ($parse) {
-      return {
-          require: 'ngModel',
-          restrict: 'A',
-          link: function ($scope, $element, $attrs, ngModel) {
-              ngModel.$setViewValue($parse($attrs.ngModelInit)($scope));
-              ngModel.$render();
-              $scope.$destroy();
-          }
-      };
-  }]);
+    var app = angular.module('ngModelInit');
+
+    app.directive('ngModelInit', ['$parse', function ($parse) {
+        return {
+            require: 'ngModel',
+            restrict: 'A',
+            link: function ($scope, $element, $attrs, ngModel) {
+                ngModel.$setViewValue($parse($attrs.ngModelInit)($scope));
+                ngModel.$render();
+                $scope.$destroy();
+            }
+        };
+    }]);
 
 })(angular)
